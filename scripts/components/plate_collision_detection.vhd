a15a9c31b4034df2fef35766ff2597de2c47cf58
ZXCroon <zxcroon@gmail.com>
2017-05-26 03:18:55 +0800

    corner of plate_collision_detection

--- scripts/components/plate_collision_detection.vhd
+++ scripts/components/plate_collision_detection.vhd
@@ -14,12 +14,18 @@ entity plate_collision_detection is
 end plate_collision_detection;
 
 architecture bhv of plate_collision_detection is
-	signal top_side, inside_x_range: boolean := false;
+	signal top_side, inside_x_range: boolean;
+	signal l_corner, r_corner: boolean;
+	signal radius2: integer;
 begin
 	top_side <= ((plate.l_position(1) - ball.position(1)) <= ball.radius);
 	inside_x_range <= (ball.position(0) >= plate.l_position(0) and
 	                   ball.position(0) <= plate.l_position(0) + plate.len);
-	collision <= horizontal when (top_side and inside_x_range) else none;
-	-- corner-collision --
+	
+	radius2 <= ball.radius * ball.radius;
+	l_corner <= distance2(plate.l_position, ball.position) <= radius2;
+	r_corner <= distance2(plate.l_position + construct_vector(plate.len, 0), ball.position) <= radius2;
+	collision <= corner when (l_corner or r_corner) else
+	             horizontal when (top_side and inside_x_range) else none;
 	fall_out <= (ball.position(1) > plate.l_position(1));
 end bhv;

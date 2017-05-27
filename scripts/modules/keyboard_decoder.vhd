ce1dbdfcd03d2f4b970938595b357f7a3c8a0283
吴启凌 <wuqiling97@163.com>
2017-05-27 18:06:07 +0800

    将原来的keyboard debug文件迁移过来了

--- scripts/modules/keyboard_decoder.vhd
+++ scripts/modules/keyboard_decoder.vhd
@@ -7,8 +7,8 @@ use work.key_coding.all;
 entity keyboard_decoder is
 port(
 	-- datain: ps2data; clkin: ps2 clock; fclk: filter clock;
-	datain,clkin,fclk,rst_in: in std_logic;
-	board_speed: out integer;
+	datain, clkin, fclk, rst_in: in std_logic;
+	board_speed: out integer range -2 to 2;
 	-- 游戏流程的确认，取消
 	confirm, quit, upp, downp: out std_logic
 );
@@ -19,6 +19,7 @@ architecture behave of keyboard_decoder is
 	port (
 		datain, clkin : in std_logic ; -- PS2 clk and data
 		fclk, rst : in std_logic ;  -- filter clock
+		dataok : buffer std_logic ;  -- data output enable signal
 		scancode : out std_logic_vector(7 downto 0) -- scan code signal output
 	);
 	end component;
@@ -27,48 +28,53 @@ architecture behave of keyboard_decoder is
 	signal rst : std_logic;
 	-- p stands for pressed
 	signal leftp, rightp, shiftp: std_logic := '0';
+	signal isbreak : std_logic := '0';
+	signal dataok: std_logic;
 begin
 	rst<=not rst_in;
-	kb: KeyboardReader port map(datain,clkin,fclk,rst,scancode);
 	
-	scancode_decoder: process(scancode, fclk) is
-		variable isbreak: std_logic := '0';
+	kb: KeyboardReader port map(datain, clkin, fclk, rst, dataok, scancode);
+	
+	scancode_decoder: process(scancode, fclk, rst, dataok) is
+--		variable isbreak: std_logic := '0';
 	begin
-		if(rising_edge(fclk)) then
-			lastscancode <= scancode;
-			if(scancode /= lastscancode) then
-				case scancode is
-					when key_break => isbreak := '1';
-					when key_left =>
-						leftp <= not isbreak;
-						isbreak := '0';
-					when key_right =>
-						rightp <= not isbreak;
-						isbreak := '0';
-					when key_up =>
-						upp <= not isbreak;
-						isbreak := '0';
-					when key_down =>
-						downp <= not isbreak;
-						isbreak := '0';
-					when key_lshift =>
-						shiftp <= not isbreak;
-						isbreak := '0';
-					when key_z =>
-						confirm <= not isbreak;
-						isbreak := '0';
-					when key_x =>
-						quit <= not isbreak;
-						isbreak := '0';
-					when others =>
-				end case;
-			end if;
+		-- dataok 0->1 means scancode has been updated
+		if (rst='1') then
+			leftp <= '0'; rightp <= '0'; shiftp <= '0';
+			upp <= '0'; downp <= '0'; confirm <= '0'; quit <= '0'; 
+			isbreak <= '0';
+		elsif(rising_edge(dataok)) then
+			case scancode is
+				when key_break => isbreak <= '1';
+				when key_up =>
+					upp <= not isbreak;
+					isbreak <= '0';
+				when key_down =>
+					downp <= not isbreak;
+					isbreak <= '0';
+				when key_left =>
+					leftp <= not isbreak;
+					isbreak <= '0';
+				when key_right =>
+					rightp <= not isbreak;
+					isbreak <= '0';
+				when key_lshift =>
+					shiftp <= not isbreak;
+					isbreak <= '0';
+				when key_z =>
+					confirm <= not isbreak;
+					isbreak <= '0';
+				when key_x =>
+					quit <= not isbreak;
+					isbreak <= '0';
+				when others => null;
+			end case;
 		end if;
 	end process;
 	
 	speed_converter: process(leftp, rightp, shiftp) is
-		constant lowspeed: integer := 100;
-		constant highspeed: integer := 50;
+		constant lowspeed: integer := 1;
+		constant highspeed: integer := 2;
 		variable link: std_logic_vector(1 downto 0);
 	begin
 		link := leftp & rightp;
@@ -77,12 +83,14 @@ begin
 				when "00"|"11" => board_speed <= 0;
 				when "01" => board_speed <= highspeed;
 				when "10" => board_speed <= -highspeed;
+				when others => null;
 			end case;
 		else
 			case link is
 				when "00"|"11" => board_speed <= 0;
 				when "01" => board_speed <= lowspeed;
 				when "10" => board_speed <= -lowspeed;
+				when others => null;
 			end case;
 		end if;
 	end process;

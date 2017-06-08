3ffba6920762c14cb606b74acd40dc9be7c7d161
吴启凌 <wuqiling97@163.com>
2017-06-08 18:34:15 +0800

    Revert "pause and over message display; bug of bricks display"
    This reverts commit 0cd63a1bc631c461e6600f9bfef32c9e57ea3f38.

--- scripts/rom_comp/img_reader2v.vhd
+++ scripts/rom_comp/img_reader2v.vhd
@@ -2,33 +2,32 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.std_logic_arith.all;
-use work.img_coding.all;
+use work.img_coding2v.all;
 
 -- 使用方式
 -- 在rising_edge(dataok)时, 读取romdata, 并且准备好下一个请求的address
 -- 之后在间隔一个clk跳变之后即可读取到新的data
 
-entity img_reader is
+entity img_reader2v is
 	port (
 		x: in std_logic_vector(9 downto 0);
 		y: in std_logic_vector(8 downto 0);
-		img: in img_info;
+		img2v: in img_info2v;
 		-- outclk frequnceny = inclk freq * 2
 		inclk: in std_logic;
 		clken: in std_logic;
-		r, g, b: out std_logic_vector(2 downto 0);
+		filled: out std_logic;
 		dataok: out std_logic := '0'
 	);
-end img_reader;
+end img_reader2v;
 
-architecture bhv of img_reader is
-	component rom_reader
+architecture bhv of img_reader2v is
+	component rom_reader_1
 		PORT
 		(
 			address		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-			clken		: IN STD_LOGIC  := '1';
 			clock		: IN STD_LOGIC  := '1';
-			q		: OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
+			q		: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
 		);
 	end component;
 
@@ -36,15 +35,13 @@ architecture bhv of img_reader is
 	signal state: statetype := read_add;
 
 	signal address: std_logic_vector(15 downto 0) := (others=>'0');
-	signal romdata: std_logic_vector(8 downto 0);
+	signal romdata: std_logic_vector(0 downto 0);
 
 	signal addint: integer range 0 to 2**address'length-1 := 0;
 begin
-	romreader: rom_reader port map(address, clken, inclk, romdata);
+	romreader: rom_reader_1 port map(address, inclk, romdata);
 	address <= conv_std_logic_vector(addint, address'length);
-	r <= romdata(8 downto 6);
-	g <= romdata(5 downto 3);
-	b <= romdata(2 downto 0);
+	filled <= romdata(0);
 
 	state_machine: process(state, inclk)
 	begin
@@ -71,11 +68,11 @@ begin
 		end if;
 	end process;
 
-	data_address: process(x, y, img)
+	data_address: process(x, y, img2v)
 		variable xint, yint: integer; -- range addint'low to addint'high;
 	begin
 		xint := conv_integer(x);
 		yint := conv_integer(y);
-		addint <= offset_dic(img) + xint + yint * width_dic(img);
+		addint <= offset_dic2v(img2v) + xint + yint * width_dic2v(img2v);
 	end process;
 end bhv;
--- scripts/rom_comp/rom_reader.vhd
+++ scripts/rom_comp/rom_reader.vhd
@@ -42,7 +42,7 @@ USE altera_mf.all;
 ENTITY rom_reader IS
 	PORT
 	(
-		address		: IN STD_LOGIC_VECTOR (16 DOWNTO 0);
+		address		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 		clken		: IN STD_LOGIC  := '1';
 		clock		: IN STD_LOGIC  := '1';
 		q		: OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
@@ -73,7 +73,7 @@ ARCHITECTURE SYN OF rom_reader IS
 		width_byteena_a		: NATURAL
 	);
 	PORT (
-			address_a	: IN STD_LOGIC_VECTOR (16 DOWNTO 0);
+			address_a	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 			clock0	: IN STD_LOGIC ;
 			clocken0	: IN STD_LOGIC ;
 			q_a	: OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
@@ -87,15 +87,15 @@ BEGIN
 	GENERIC MAP (
 		clock_enable_input_a => "NORMAL",
 		clock_enable_output_a => "NORMAL",
-		init_file => "../../images/mif/allimg.mif",
+		init_file => "./images/mif/allimg.mif",
 		intended_device_family => "Cyclone II",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
-		numwords_a => 100000,
+		numwords_a => 65536,
 		operation_mode => "ROM",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "CLOCK0",
-		widthad_a => 17,
+		widthad_a => 16,
 		width_a => 9,
 		width_byteena_a => 1
 	)
@@ -130,36 +130,36 @@ END SYN;
 -- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 -- Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "../../images/mif/allimg.mif"
--- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "100000"
+-- Retrieval info: PRIVATE: MIFfilename STRING "./images/mif/allimg.mif"
+-- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "65536"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
 -- Retrieval info: PRIVATE: RegOutput NUMERIC "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 -- Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
--- Retrieval info: PRIVATE: WidthAddr NUMERIC "17"
+-- Retrieval info: PRIVATE: WidthAddr NUMERIC "16"
 -- Retrieval info: PRIVATE: WidthData NUMERIC "9"
 -- Retrieval info: PRIVATE: rden NUMERIC "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "NORMAL"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "NORMAL"
--- Retrieval info: CONSTANT: INIT_FILE STRING "../../images/mif/allimg.mif"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "./images/mif/allimg.mif"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "100000"
+-- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "65536"
 -- Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 -- Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
--- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "17"
+-- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "16"
 -- Retrieval info: CONSTANT: WIDTH_A NUMERIC "9"
 -- Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
--- Retrieval info: USED_PORT: address 0 0 17 0 INPUT NODEFVAL "address[16..0]"
+-- Retrieval info: USED_PORT: address 0 0 16 0 INPUT NODEFVAL "address[15..0]"
 -- Retrieval info: USED_PORT: clken 0 0 0 0 INPUT VCC "clken"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
 -- Retrieval info: USED_PORT: q 0 0 9 0 OUTPUT NODEFVAL "q[8..0]"
--- Retrieval info: CONNECT: @address_a 0 0 17 0 address 0 0 17 0
+-- Retrieval info: CONNECT: @address_a 0 0 16 0 address 0 0 16 0
 -- Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: @clocken0 0 0 0 0 clken 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 9 0 @q_a 0 0 9 0

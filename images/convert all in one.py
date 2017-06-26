6ecaada78e290b1b8c49c452650d1c8ba49dfc86
吴启凌 <wuqiling97@163.com>
2017-06-26 23:17:17 +0800

    add comment for mif converter

--- images/convert all in one.py
+++ images/convert all in one.py
@@ -4,6 +4,7 @@ import os
 
 
 def bin3(x):
+    """将[0, 7]的十进制数转为二进制"""
     s = bin(x)[2:]
     if len(s) == 0:
         return '000'
@@ -17,9 +18,9 @@ def bin3(x):
 filelst = os.listdir('.')
 filelst.sort()
 
-offset = {}
-width = {}
-datalst = []
+offset = {}  # 图片起始位置
+width = {}   # 图片宽度
+datalst = [] # mif文件内容，按行存储
 
 for filename in filelst:
     if filename.endswith('.jpg') and not filename.endswith('_2v.jpg'):
@@ -28,13 +29,13 @@ for filename in filelst:
 
         txt = ''
         txt += 'WIDTH=9;\nDEPTH=%d;\nADDRESS_RADIX=UNS;\nDATA_RADIX=BIN;\nCONTENT BEGIN\n' % (w * h)
-        # offset.append('\tconstant {name}: integer := {pos};'.format(name=filename.replace('.jpg', '_start'), pos=len(datalst)))
         offset[filename.replace('.jpg', '')] = len(datalst)
         width[filename.replace('.jpg', '')] = w
 
         for i in range(h):
             for j in range(w):
                 r, g, b = img.getpixel((j, i))
+                # 将[0, 255]的颜色转为[0, 7]*32
                 r = r // 32 * 32
                 g = g // 32 * 32
                 b = b // 32 * 32
@@ -44,19 +45,22 @@ for filename in filelst:
         img.save('jpg3/' + filename.replace('.jpg', '.png'))
 
 
-fout = open('mif/allimg.mif', 'w')
+fmif = open('mif/allimg.mif', 'w')
+# img_coding_temp 为一个字符串模板，通过str.format生成实际内容
 foffset = open('../scripts/packages/img_coding_temp.vhd')
 template = foffset.read()
 foffset.close()
 foffset = open('../scripts/packages/img_coding.vhd', 'w')
 
-fout.write('WIDTH=9;\nDEPTH={};\nADDRESS_RADIX=UNS;\nDATA_RADIX=BIN;\nCONTENT BEGIN\n'.format(len(datalst)))
+# 写mif文件头中的概述信息
+fmif.write('WIDTH=9;\nDEPTH={};\nADDRESS_RADIX=UNS;\nDATA_RADIX=BIN;\nCONTENT BEGIN\n'.format(len(datalst)))
 for line in datalst:
-    fout.write(line)
-fout.write('END;\n')
+    fmif.write(line)
+fmif.write('END;\n')
 
 keys = list(offset.keys())
 keys.sort()
+# 将offset, width中的信息格式化并输出
 offset_txt = ',\n\t\t'.join(map(lambda k: '{} => {}'.format(k, offset[k]), keys))
 width_txt = ',\n\t\t'.join(map(lambda k: '{} => {}'.format(k, width[k]), keys))
 
@@ -65,6 +69,7 @@ foffset.write(template.format(img_info=img_info, offset=offset_txt, width=width_
 
 
 ####################################################################
+# 2值图像的转化（实际未用到）
 
 offset = {}
 width = {}
